f500d192fe87c6b823d51d7d77fbdefcea94f8aa
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-07 07:07:59 +0300

    C2.22-26 Create first Model, connection string, DbContext

--- BullWeb/Program.cs
+++ BullWeb/Program.cs
@@ -1,7 +1,12 @@
+using BullWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(o =>
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
