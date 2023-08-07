89e585a950361442d463991d94048c01b58a4edb
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-07 20:41:33 +0300

    C2.27-28 Create Database and Category table

--- BullWeb/Data/ApplicationDbContext.cs
+++ BullWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BullWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BullWeb.Data;
 
@@ -6,6 +7,7 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
     {
-        
     }
+
+    public DbSet<Category> Categories { get; set; }
 }
