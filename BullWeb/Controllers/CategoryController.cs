e62c14dd9934455fb77fe5569f343cbd86dc70c3
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-07 20:51:20 +0300

    C2.29 Create Category Controller

---
+++ BullWeb/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BullWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
