4d2ac083362583571a3231ad614612fe0395c303
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-07 21:03:03 +0300

    C2.31 Category Table Seed

--- BullWeb/Migrations/20230807180023_SeedCategoryTable.Designer.cs
+++ BullWeb/Migrations/20230807180023_SeedCategoryTable.Designer.cs
@@ -3,6 +3,7 @@ using BullWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BullWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230807180023_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
