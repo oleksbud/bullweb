b4651a232857506d26151c0971688553e674e4b6
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-08 11:10:11 +0300

    Category CRUD Operations (#1)
    * C2.22-26 Create first Model, connection string, DbContext
    
    * C2.27-28 Create Database and Category table
    
    * C2.29 Create Category Controller
    
    * C2.30 Add Category link in header
    
    * C2.31 Category Table Seed
    
    * C2.32-34 Get and display all categories
    
    * C2.35 Bootswatch theme and bootstrap icons
    
    * C2.36 Add the button for new category
    
    * C2.37 UI for Create Category
    
    * C2.38 Input tag Helpers
    
    * C2.39 Create DB record in Category table by an input values
    
    * C2.40 Server side validation
    
    * C2.41 Custom validation
    
    * C2.42 False asp-validation-summary step
    
    * C2.43 Client side validation
    
    * C2.44-46 Add Edit button and Update Category
    
    * C2.47-48 Delete UI and functionality
    
    * C2.50 TempData
    
    * C2.51 Partial view

---
+++ BullWeb/Controllers/CategoryController.cs
@@ -0,0 +1,101 @@
+﻿using BullWeb.Data;
+using BullWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BullWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoryController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public IActionResult Index()
+    {
+        var categoryList = _context.Categories.ToList();
+        return View(categoryList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category has created successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var category = _context.Categories.FirstOrDefault(x => x.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category has updated successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var category = _context.Categories.FirstOrDefault(x => x.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+    
+    [HttpPost, ActionName("Delete")]
+    public IActionResult UltimateDelete(int? id)
+    {
+        var category = _context.Categories.FirstOrDefault(x => x.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        _context.Categories.Remove(category);
+        _context.SaveChanges();
+        TempData["success"] = "Category has deleted successfully";
+        return RedirectToAction("Index");
+    }
+}
