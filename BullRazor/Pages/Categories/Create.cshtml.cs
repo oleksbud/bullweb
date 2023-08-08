ea967ce1ac6f014866cd7cbc1dc149a1f0bd7b02
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-08 21:06:21 +0300

    C3.58 UI for Create Category

---
+++ BullRazor/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using BullRazor.Data;
+using BullRazor.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BullRazor.Pages.Categories;
+
+public class Create : PageModel
+{
+    private readonly ApplicationDbContext _context;
+    public Category Category { get; set; }
+
+    public Create(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public void OnGet()
+    {
+        
+    }
+}
