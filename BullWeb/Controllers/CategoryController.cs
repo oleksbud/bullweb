ac66bf6b979cb579af2559062f436afb1c43ae21
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-08 10:36:19 +0300

    C2.44-46 Add Edit button and Update Category

--- BullWeb/Controllers/CategoryController.cs
+++ BullWeb/Controllers/CategoryController.cs
@@ -36,4 +36,33 @@ public class CategoryController : Controller
 
         return View();
     }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var category = _context.Categories.FirstOrDefault(x => x.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
 }
