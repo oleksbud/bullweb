9f3131333215dfa3cc9313fe70b15c2594e8d316
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-08 21:57:19 +0300

    C3.60 UI for Edit and Delete Categoryu

---
+++ BullRazor/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using BullRazor.Data;
+using BullRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BullRazor.Pages.Categories;
+
+public class Delete : PageModel
+{
+    private readonly ApplicationDbContext _context;
+    [BindProperty]
+    public Category? Category { get; set; }
+
+    public Delete(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public void OnGet(int? id)
+    {
+        if (id != null && id != 0)
+        {
+            Category = _context.Categories.Find(id);
+        }
+    }
+
+    public IActionResult OnPost()
+    {
+        var category = _context.Categories.FirstOrDefault(x => x.Id == Category.Id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        _context.Categories.Remove(category);
+        _context.SaveChanges();
+        return RedirectToPage("Index");
+    }
+}
