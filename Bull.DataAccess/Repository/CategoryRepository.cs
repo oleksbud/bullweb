ec011f61e2e8cae35403f57dc103b45a9d8b1f62
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-09 10:35:55 +0300

    C5.72 Implement category repository

---
+++ Bull.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using Bull.DataAccess.Data;
+using Bull.DataAccess.Repository.IRepository;
+using Bull.Models.Models;
+
+namespace Bull.DataAccess.Repository;
+
+public class CategoryRepository : Repository<Category>, ICategoryRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoryRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+    
+    public void Update(Category category)
+    {
+        _context.Update(category);
+    }
+
+    public void Save()
+    {
+        _context.SaveChanges();
+    }
+}
