23ff1bb178a9ea7bef2b45fd2c81ca201b5fb400
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-09 10:30:55 +0300

    C5.71 The repository interface for Category

---
+++ Bull.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Bull.Models.Models;
+
+namespace Bull.DataAccess.Repository.IRepository;
+
+public interface ICategoryRepository: IRepository<Category>
+{
+    void Update(Category category);
+    void Save();
+}
