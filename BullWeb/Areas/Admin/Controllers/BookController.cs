a829f5d09b16f3f2f586fea8cfda925b71b32d9f
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-13 10:56:48 +0300

    C6.84 CRUD for Book

---
+++ BullWeb/Areas/Admin/Controllers/BookController.cs
@@ -0,0 +1,101 @@
+﻿using Bull.DataAccess.Repository.IRepository;
+using Bull.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BullWeb.Areas.Admin.Controllers;
+[Area("Admin")]
+public class BookController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public BookController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    
+    public IActionResult Index()
+    {
+        var books = _unitOfWork.BookRepository.GetAll().ToList();
+        return View(books);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Book book)
+    {
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.BookRepository.Add(book);
+            _unitOfWork.Save();
+            TempData["success"] = "Book has created successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var book = _unitOfWork.BookRepository.Get(x => x.Id == id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        return View(book);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Book book)
+    {
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.BookRepository.Update(book);
+            _unitOfWork.Save();
+            TempData["success"] = "Book has updated successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var book = _unitOfWork.BookRepository.Get(x => x.Id == id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        return View(book);
+    }
+    
+    [HttpPost, ActionName("Delete")]
+    public IActionResult UltimateDelete(int? id)
+    {
+        var book = _unitOfWork.BookRepository.Get(x => x.Id == id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        _unitOfWork.BookRepository.Remove(book);
+        _unitOfWork.Save();
+        TempData["success"] = "Book has deleted successfully";
+        return RedirectToAction("Index");
+    }
+}
