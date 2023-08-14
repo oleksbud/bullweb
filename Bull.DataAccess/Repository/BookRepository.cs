81ce935100699348353c7384ca179a4f2d9af020
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-14 09:34:14 +0300

    C6.97 Custom code in update.

--- Bull.DataAccess/Repository/BookRepository.cs
+++ Bull.DataAccess/Repository/BookRepository.cs
@@ -16,6 +16,23 @@ public class BookRepository : Repository<Book>, IBookRepository
     
     public void Update(Book book)
     {
-        _context.Update(book);
+        var storedBook = _context.Books.FirstOrDefault(x => x.Id == book.Id);
+        if (storedBook != null)
+        {
+            storedBook.Title = book.Title;
+            storedBook.Isbn = book.Isbn;
+            storedBook.Author = book.Author;
+            storedBook.Description = book.Description;
+            storedBook.CategoryId = book.CategoryId;
+            storedBook.ListPrice = book.ListPrice;
+            storedBook.Price = book.Price;
+            storedBook.Price50 = book.Price50;
+            storedBook.Price100 = book.Price100;
+            
+            if (book.ImageUrl != null)
+            {
+                storedBook.ImageUrl = book.ImageUrl;
+            }
+        }
     }
 }
