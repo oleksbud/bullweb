db1b6a66b2d4c2632aa2a0a0b575af2802232c8a
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-14 21:35:40 +0300

    C7.105 Home page

--- BullWeb/Areas/Customer/Controllers/HomeController.cs
+++ BullWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Bull.DataAccess.Repository.IRepository;
 using Bull.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,15 +8,19 @@ namespace BullWeb.Areas.Customer.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var dictionary = new List<string> { "Category" };
+        IEnumerable<Book> books = _unitOfWork.BookRepository.GetAll(dictionary);
+        return View(books);
     }
 
     public IActionResult Privacy()
