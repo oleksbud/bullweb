08240807e9d748eb0db1b001b8ddbaad100ccfda
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-14 17:00:02 +0300

    C6.98 Loading navigation properties

--- Bull.DataAccess/Repository/IRepository/IRepository.cs
+++ Bull.DataAccess/Repository/IRepository/IRepository.cs
@@ -5,7 +5,9 @@ namespace Bull.DataAccess.Repository.IRepository;
 public interface IRepository<T> where T : class
 {
     IEnumerable<T> GetAll();
+    IEnumerable<T> GetAll(List<string> includeProperties);
     T? Get(Expression<Func<T, bool>> filter);
+    T? Get(Expression<Func<T, bool>> filter, List<string> includeProperties);
     void Add(T entity);
     void Remove(T entity);
     void RemoveRange(IEnumerable<T> entities);
