f9efbc0d3acbae31309042281aaef309303a88bb
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-15 11:14:43 +0300

    C8.109 Fix Identity scaffolding issue

--- Bull.DataAccess/Data/ApplicationDbContext.cs
+++ Bull.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Bull.Models.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bull.DataAccess.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
     {
