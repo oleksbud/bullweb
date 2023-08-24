af0932d4ecb9f2711e3e93949eb5555bb5e25634
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-24 11:13:03 +0300

    C10. Add a Shopping Cart repository

--- Bull.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bull.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -5,6 +5,7 @@ public interface IUnitOfWork
     ICategoryRepository CategoryRepository { get;  }
     IBookRepository BookRepository { get; }
     ICompanyRepository CompanyRepository { get; }
+    IShoppingCartRepository ShoppingCartRepository { get; }
 
     public void Save();
 }
