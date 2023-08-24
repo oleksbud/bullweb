6fb5ccdb4c62c72dd27ea348f1deace062bf61c7
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-24 11:17:18 +0300

    C10.129 Add an Application User Repository

--- Bull.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bull.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -6,6 +6,7 @@ public interface IUnitOfWork
     IBookRepository BookRepository { get; }
     ICompanyRepository CompanyRepository { get; }
     IShoppingCartRepository ShoppingCartRepository { get; }
+    IApplicationUserRepository ApplicationUserRepository { get; }
 
     public void Save();
 }
