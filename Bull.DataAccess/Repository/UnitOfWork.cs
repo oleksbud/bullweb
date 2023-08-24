98b2622f3b01b957ae75256c619aadc2a8e073f3
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-24 11:13:03 +0300

    C10. Add a Shopping Cart repository

--- Bull.DataAccess/Repository/UnitOfWork.cs
+++ Bull.DataAccess/Repository/UnitOfWork.cs
@@ -8,7 +8,8 @@ public class UnitOfWork : IUnitOfWork
     private readonly ApplicationDbContext _context;
     public ICategoryRepository CategoryRepository { get; private set; }
     public IBookRepository BookRepository { get; private set; }
-    public ICompanyRepository CompanyRepository { get; }
+    public ICompanyRepository CompanyRepository { get; private set;}
+    public IShoppingCartRepository ShoppingCartRepository { get; private set;}
 
     public UnitOfWork(ApplicationDbContext context)
     {
@@ -16,6 +17,7 @@ public class UnitOfWork : IUnitOfWork
         CategoryRepository = new CategoryRepository(_context);
         BookRepository = new BookRepository(_context);
         CompanyRepository = new CompanyRepository(_context);
+        ShoppingCartRepository = new ShoppingCartRepository(_context);
     }
     
     public void Save()
