f8e03e0a881eeb05e57d2578bdfb3fea2ad7cac1
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-08-25 09:09:22 +0300

    C10.134 Shopping Cart UI

---
+++ BullWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BullWeb.Areas.Customer.Controllers;
+
+[Area("Customer")]
+public class CartController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
