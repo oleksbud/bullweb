6340b82d9c7f64118e75d8be6c97d273971855b4
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-09-02 09:09:25 +0300

    C10.136 Get Order Total for shopping cart

--- Bull.Models/Models/ShoppingCart.cs
+++ Bull.Models/Models/ShoppingCart.cs
@@ -17,4 +17,6 @@ public class ShoppingCart
     [ForeignKey("ApplicationUserId")]
     [ValidateNever]
     public ApplicationUser ApplicationUser { get; set; }
+    [NotMapped]
+    public double Price { get; set; }
 }
