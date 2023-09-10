8368379b8339373828dedc8b06fbb3850302f348
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-09-10 07:39:00 +0300

    C11.141 Add the models for OrderHeader and OrderDetail

---
+++ Bull.Models/Models/OrderDetail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Bull.Models.Models;
+
+public class OrderDetail
+{
+    public int Id { get; set; }
+    [Required]
+    public int OrderHeaderId { get; set; }
+    [ForeignKey("OrderHeaderId")]
+    [ValidateNever]
+    public OrderHeader OrderHeader { get; set; }
+
+    public int BookId { get; set; }
+    [ForeignKey("BookId")]
+    [ValidateNever]
+    public Book Book { get; set; }
+
+    public int Count { get; set; }
+    public double Price { get; set; }
+}
