581a22fef648517151fd5be9b214a4bbad952f83
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-10-15 11:48:55 +0300

    C11.151 Add Helper method for updating statuses of payment and order

--- Bull.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ Bull.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -5,4 +5,6 @@ namespace Bull.DataAccess.Repository.IRepository;
 public interface IOrderHeaderRepository: IRepository<OrderHeader>
 {
     void Update(OrderHeader orderHeader);
+    void UpdateStatuses(int id, string orderStatus, string? paymentStatus = null);
+    void UpdateStripePaymentId(int id, string sessionId, string paymentIdentId);
 }
