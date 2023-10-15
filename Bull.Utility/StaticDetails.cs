64c9d1ab5c48b7901768051757a799adfd973561
Oleksandr Budnytskyy <oleksbud@gmail.com>
2023-10-15 10:14:29 +0300

    C11.146 Order Statuses

--- Bull.Utility/StaticDetails.cs
+++ Bull.Utility/StaticDetails.cs
@@ -6,4 +6,16 @@ public static class StaticDetails
     public const string RoleCompany = "Company";
     public const string RoleAdmin = "Admin";
     public const string RoleEmployee = "Employee";
+    
+    public const string StatusPending = "Pending";
+    public const string StatusApproved = "Approved";
+    public const string StatusInProcess = "Processing";
+    public const string StatusShipped = "Shipped";
+    public const string StatusCancelled = "Cancelled";
+    public const string StatusRefunded = "Refunded";
+    
+    public const string PaymentStatusPending = "Pending";
+    public const string PaymentStatusApproved = "Approved";
+    public const string PaymentStatusDelayedPayment = "Approved For Delayed Payment";
+    public const string PaymentStatusRejected = "Rejected";
 }
